78a51f6730abd26326cd09c6885bb57dccbaa483
Key Hoffman <key.hoffman.dev@gmail.com>
2025-01-11 14:53:12 -0600

    refactor: convert quotes to single quotes in TypeScript files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,36 +3,36 @@ import { CheckCircleIcon } from '@chakra-ui/icons'
 
 export default function Home() {
   return (
-    <Box as="main" py={8}>
-      <Container maxW="2xl">
-        <VStack spacing={6} align="stretch">
-          <Heading as="h1" size="2xl">
+    <Box as='main' py={8}>
+      <Container maxW='2xl'>
+        <VStack spacing={6} align='stretch'>
+          <Heading as='h1' size='2xl'>
             Welcome to Claude Test Next
           </Heading>
           
-          <Text fontSize="lg">
+          <Text fontSize='lg'>
             This is a Next.js application created with TypeScript and Chakra UI.
           </Text>
 
-          <Box bg="gray.50" p={6} borderRadius="lg" shadow="sm">
-            <Heading as="h2" size="lg" mb={4}>
+          <Box bg='gray.50' p={6} borderRadius='lg' shadow='sm'>
+            <Heading as='h2' size='lg' mb={4}>
               Features
             </Heading>
             <List spacing={3}>
               <ListItem>
-                <ListIcon as={CheckCircleIcon} color="green.500" />
+                <ListIcon as={CheckCircleIcon} color='green.500' />
                 Next.js 14 with App Router
               </ListItem>
               <ListItem>
-                <ListIcon as={CheckCircleIcon} color="green.500" />
+                <ListIcon as={CheckCircleIcon} color='green.500' />
                 TypeScript Configuration
               </ListItem>
               <ListItem>
-                <ListIcon as={CheckCircleIcon} color="green.500" />
+                <ListIcon as={CheckCircleIcon} color='green.500' />
                 React 18
               </ListItem>
               <ListItem>
-                <ListIcon as={CheckCircleIcon} color="green.500" />
+                <ListIcon as={CheckCircleIcon} color='green.500' />
                 Chakra UI Components
               </ListItem>
             </List>
--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,4 +1,4 @@
-"use client"
+'use client'
 
 import { ChakraProvider } from '@chakra-ui/react'
 
